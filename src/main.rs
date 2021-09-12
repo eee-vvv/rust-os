25573c6b137189da08019f4ce0de432d3573708e
eeevvv <evan.w.and@gmail.com>
2021-09-11 23:30:51 -0400

    allow kernel to access page table memory addresses

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,35 @@ mod vga_buffer;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello Providence!");
+    use rust_os::memory::active_level_4_table;
+    use x86_64::VirtAddr;
 
+    println!("Hello Providence!");
     rust_os::init();
 
-    use x86_64::registers::control::Cr3; 
-    
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    for (i, entry) in l4_table.iter().enumerate() {
+        use x86_64::structures::paging::PageTable;
+
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
+
+            // get and convert the physical address from the entry
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // print nonempty entries of the level 3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
+        }
+    }
 
     #[cfg(test)]
     test_main();
