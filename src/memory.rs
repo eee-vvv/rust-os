58c4a440065831f765261f41cba77e77f58b18d2
eeevvv <evan.w.and@gmail.com>
2021-09-12 23:25:54 -0400

    bring in x86_64 crate OffsetPageTable struct

--- src/memory.rs
+++ src/memory.rs
@@ -1,17 +1,28 @@
 use x86_64::{
-    structures::paging::PageTable,
-    VirtAddr,
+    structures::paging::{OffsetPageTable, PageTable},
+    PhysAddr, VirtAddr,
 };
 
-/// Returns a mutable reference to the active level 4 table.
+///Initializes a new OffsetPageTable.
 ///
+///# Safety
 /// This function is unsafe because the caller must guarantee that the
 /// complete physical memory is mapped to virtual memory at the passed
 /// `physical_memory_offset`. Also, this function must be only called once
 /// to avoid aliasing `&mut` references (which is undefined behavior).
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
-    -> &'static mut PageTable
-{
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+/// Returns a mutable reference to the active level 4 table.
+///
+/// # Safety
+/// This function is unsafe because the caller must guarantee that the
+/// complete physical memory is mapped to virtual memory at the passed
+/// `physical_memory_offset`. Also, this function must be only called once
+/// to avoid aliasing `&mut` references (which is undefined behavior).
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
